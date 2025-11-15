8ee201c7b4a0ba359181bfdb228b80b45bf1b390
robert <robertn702@gmail.com>
2025-11-15 09:38:48 -0500

    chore: release version 0.16.0

--- src/constants.ts
+++ src/constants.ts
@@ -4,6 +4,6 @@
  * IMPORTANT: Keep VERSION in sync with package.json version
  */
 
-export const VERSION = "0.15.4";
+export const VERSION = "0.16.0";
 export const SERVER_NAME = "Sunsama API Server";
 export const PACKAGE_NAME = "mcp-sunsama";
